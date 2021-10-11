7d05ed387338dbb28f49045af997a33379711bef
chaowen guo <chaowen.guo1@gmail.com>
2021-10-10 20:23:36 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,4 +1,4 @@
-import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, uvloop, builtins
+import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, uvloop, builtins, ssl
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def database(app):
