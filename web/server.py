a58b2bf8d94c3ec1fcd9f2550fcb668a9fd9f20b
chaowen guo <chaowen.guo1@gmail.com>
2021-10-13 18:08:53 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -3,7 +3,7 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def database(app):
     app.setdefault('database', await asyncpg.create_pool(host='postgrespostgres.postgres.database.azure.com', user='postgres', database='default', password='pos1gres+', ssl=ssl.create_default_context(cafile='DigiCertGlobalRootCA.crt.pem')))
-    app.setdefault('cache', aredis.StrictRedis('redis').cache('cache'))
+    #app.setdefault('cache', aredis.StrictRedis('redis').cache('cache'))
     #producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka')
     #await producer.start()
     #await producer.send_and_wait('topic', b"Super message")
@@ -14,10 +14,10 @@ async def database(app):
 async def ajax(request):
     body = await request.json()
     body = ' '.join(' '.join((key, builtins.str(value))) for key,value in body.items())
-    records = await request.app.get('cache').get(body)
-    if not records:
-        async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=builtins.str)
-        await request.app.get('cache').set(body, records)
+    #records = await request.app.get('cache').get(body)
+    #if not records:
+    async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=builtins.str)
+        #await request.app.get('cache').set(body, records)
     return aiohttp.web.Response(text=records)
 
 app = aiohttp.web.Application()
