4d47ff92ce13fe036be52bd55d41229722be3e26
chaowen guo <chaowen.guo1@gmail.com>
2021-10-13 20:35:35 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,24 +1,25 @@
-import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, uvloop, builtins, ssl, aiohttp_cors
+import aiohttp.web, asyncpg, json, aioredis, aiokafka, asyncio, uvloop, builtins, ssl, aiohttp_cors
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def database(app):
     app.setdefault('database', await asyncpg.create_pool(host='postgrespostgres.postgres.database.azure.com', user='postgres', database='default', password='pos1gres+', ssl=ssl.create_default_context(cafile='DigiCertGlobalRootCA.crt.pem')))
-    app.setdefault('cache', aredis.StrictRedis('redis').cache('cache'))
+    app.setdefault('cache', await aioredis.create_redis_pool('redis://redis'))
     #producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka')
     #await producer.start()
     #await producer.send_and_wait('topic', b"Super message")
     #await producer.stop()
     yield
     await app.get('database').close()
+    app.get('database').close()
+    await app.get('database').wait_closed()
 
 async def ajax(request):
     body = await request.json()
     body = ' '.join(' '.join((key, builtins.str(value))) for key,value in body.items())
-    records = await request.app.get('cache').exist(body)
-    print(records)
-    #if not records:
-    async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=builtins.str)
-        #await request.app.get('cache').set(body, records)
+    records = await request.app.get('cache').get(body)
+    if not records:
+        async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=builtins.str)
+        await request.app.get('cache').set(body, records)
     return aiohttp.web.Response(text=records)
 
 app = aiohttp.web.Application()
