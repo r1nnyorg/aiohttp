50d6a27dcccdf65373173a1d7418e830642751cc
chaowen guo <chaowen.guo1@gmail.com>
2021-11-16 17:09:42 -0600

    Update patch.py

--- web/patch.py
+++ web/patch.py
@@ -1,2 +1,2 @@
 import fileinput, platform
-for line in fileinput.input(f'/usr/local/venv/lib/python{platform.python_version().rsplit('.', 1)[0]}/site-packages/aredis/connection.py', inplace=True): print(line.replace('loop=self.loop),', '),'), end='')
+for line in fileinput.input(f'/usr/local/venv/lib/python{platform.python_version().rsplit(".", 1)[0]}/site-packages/aredis/connection.py', inplace=True): print(line.replace('loop=self.loop),', '),'), end='')
