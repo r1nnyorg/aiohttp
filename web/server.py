6618c1079a6f293b2e953624049e869309607b0a
chaowen guo <chaowen.guo1@gmail.com>
2022-03-22 03:55:54 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -3,7 +3,7 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def database(app):
     #app.setdefault('database', await asyncpg.create_pool(host='postgrespostgres.postgres.database.azure.com', user='postgres', database='default', password='pos1gres+'))
-    app.setdefault('database', await asyncpg.create_pool(host='cockroach', user='root', database='defaultdb'))
+    app.setdefault('database', await asyncpg.create_pool(host='cockroach', user='root', database='defaultdb', port=26257))
     app.setdefault('cache', aredis.StrictRedisCluster(host='redis', port=6379, password=os.getenv('password'))) #app.setdefault('cache', aredis.StrictRedis('redis'))
     #producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka')
     #await producer.start()
