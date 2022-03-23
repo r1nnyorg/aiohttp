8a0feafb862386770bcda310ef3a0607ab2313b0
chaowen guo <chaowen.guo1@gmail.com>
2022-03-23 07:08:00 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,11 +1,11 @@
-import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, builtins, aiohttp_cors, uvloop, math, os, ssl
+import aiohttp.web, asyncpg, json, aredis, aiokafka, asyncio, builtins, aiohttp_cors, uvloop, math, os, ssl, pathlib
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def database(app):
-    #app.setdefault('database', await asyncpg.create_pool(host='postgrespostgres.postgres.database.azure.com', user='postgres', database='default', password='pos1gres+'))
-    sslctx = ssl.create_default_context(cafile='ca.crt')
+    #app.setdefault('database', await asyncpg.create_pool(host='postgrespostgres.postgres.database.azure.com', user='postgres', database='default', password='pos1gres+', ssl=True))
+    sslctx = ssl.create_default_context(cafile=pathlib.Path(__file__).resolve().parent.joinpath('ca.crt'))
     sslctx.check_hostname = False
-    sslctx.load_cert_chain('client.root.crt', keyfile='client.root.key')
+    sslctx.load_cert_chain(pathlib.Path(__file__).resolve().parent.joinpath('client.root.crt'), keyfile=pathlib.Path(__file__).resolve().parent.joinpath('client.root.key'))
     app.setdefault('database', await asyncpg.create_pool(host='cockroach', user='root', database='defaultdb', port=26257, ssl=sslctx))
     app.setdefault('cache', aredis.StrictRedisCluster(host='redis', port=6379, password=os.getenv('password'))) #app.setdefault('cache', aredis.StrictRedis('redis'))
     #producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka')
