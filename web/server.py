f454fd8e7c4f3140918136698cb14e1981b022d1
chaowen guo <chaowen.guo1@gmail.com>
2022-04-17 23:57:12 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -19,11 +19,10 @@ async def ajax(request):
     body = await request.json()
     body = ' '.join(' '.join((key, builtins.str(value))) for key,value in body.items())
     records = await request.app.get('cache').get(body)
-    print(records, type(records), flush=True)
     if not records:
         async with request.app.get('database').acquire() as connection: records = json.dumps([*map(dict, await connection.fetch(f'select * from{body}'))], default=builtins.str)
         await request.app.get('cache').set(body, records)
-    return aiohttp.web.Response(text=records)
+    return aiohttp.web.Response(text=records if builtins.isinstance(records, builtins.str) else records.decode())
 
 async def main():
     app = aiohttp.web.Application()
